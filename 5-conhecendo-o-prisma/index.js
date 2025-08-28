2ae93ea1a389789d74b2a01fcf483e65bb272eed
Carlos Gabriel <carlosgabrielweb2003@gmail.com>
2025-08-28 16:37:08 -0300

    prisma orm

---
+++ 5-conhecendo-o-prisma/index.js
@@ -0,0 +1,24 @@
+const { PrismaClient } = require("./generated/prisma")
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.post.createMany({
+    data: [
+      {
+        title: 'Olá, mundo!',
+        content: "Este é o meu primeiro post criado com a ajuda do prisma ORM!",
+        published: true
+      },
+      {
+        title: "Post 2",
+        content: null
+      }
+    ]
+  })
+}
+
+main().then(async () => {
+  const result = await prisma.post.findMany()
+  console.table(result)
+})
